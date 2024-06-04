c841cf565dccc1a785fffbee906f3bada6806580
Mica Linscheid <micascheid@gmail.com>
2024-06-03 21:22:47 -0600

    new json app

--- application.py
+++ application.py
@@ -4,15 +4,15 @@ from werkzeug.serving import run_simple
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    # app.run()
 
 
     #local stuff
-    # context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-    #
-    # # Load the cert and key, supplying the necessary passphrase
-    # context.load_cert_chain(certfile='/Users/micalinscheid/Documents/SpeechBreeze/localhttps/cert.pem',
-    #                         keyfile='/Users/micalinscheid/Documents/SpeechBreeze/localhttps/key.pem',
-    #                         password='localhttps')
-    #
-    # run_simple('0.0.0.0', 5000, app, ssl_context=context)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
+    # Load the cert and key, supplying the necessary passphrase
+    context.load_cert_chain(certfile='/Users/micalinscheid/Documents/SpeechBreeze/localhttps/cert.pem',
+                            keyfile='/Users/micalinscheid/Documents/SpeechBreeze/localhttps/key.pem',
+                            password='localhttps')
+
+    run_simple('0.0.0.0', 5000, app, ssl_context=context)
